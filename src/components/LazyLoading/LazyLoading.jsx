aa30ed79a7b03a3aeeb439c902e83ec52b1242f1
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-07-03 17:01:17 +0500

    main

---
+++ src/components/LazyLoading/LazyLoading.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LazyLoading.scss'
+
+const LazyLoading = () => {
+    return (
+        <div className="lazy_hammasi">
+            <div className='lazy'>
+            <div class="loaderAll"></div>
+        </div>
+        </div>
+    );
+}
+
+export default LazyLoading;
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React,{lazy,Suspense} from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+const  App = lazy(() => import('./App'))
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import {store} from './components/context/index.js'
+import LazyLoading from './components/LazyLoading/LazyLoading.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Suspense fallback={<LazyLoading/>}>
     <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
     </BrowserRouter>
+    </Suspense>
+   
   </React.StrictMode>,
 )
